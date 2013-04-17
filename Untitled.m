dd28df28add7a0ffc00dc37a13cc8d31eac63941
mneshati <mneshati@uni-bremen.de>
2013-04-17 19:50:22 +0200

    changed th untiltled

--- Untitled.m
+++ Untitled.m
@@ -10,15 +10,11 @@ for i1=1:axisLength
                             for i8=1:axisLength
                                 DijFullData(i1,i2,i3,i4).Q(i5,i6,i7,i8)=inf;
                                 DijFullData(i1,i2,i3,i4).U(i5,i6,i7,i8)=inf;
-                                i=i+1
                             end
                         end
                     end
                 end 
             end
-            i3
         end
-        i2
-    end
-    i1
+     end
 end
